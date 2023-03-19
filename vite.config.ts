b61d71a8e4de6f23da5bfb9d8226ffbff826c327
zhangzlun <k0926521654@gmail.com>
2023-03-19 17:44:31 +0800

    (update) test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    reactivityTransform: true
+  })],
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
